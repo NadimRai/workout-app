cba9ef0bd6f0e04513b376cce45596ad60a83457
NadimRai <rai_nadim@yahoo.com>
2016-11-05 17:55:39 -0700

    Add friendship and follow user

--- config/routes.rb
+++ config/routes.rb
@@ -6,11 +6,13 @@ Rails.application.routes.draw do
     resources :exercises
   end
 
-  resource :dashboard, only: [:index] do 
-    collection do 
+  resource :dashboard, only: [:index] do
+    collection do
       post :search, to: 'dashboard#search'
     end
   end
+
+  resources :friendships, only:[:show, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
